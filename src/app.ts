b4856a93ed23f33f4ea629d26df542a002757010
MrJonkoRisin <93263299+MrJonkoRisin@users.noreply.github.com>
2023-06-08 10:43:15 +0200

    Refactored with method desctiptions

--- src/app.ts
+++ src/app.ts
@@ -5,45 +5,98 @@ import createHttpError, { isHttpError } from "http-errors";
 dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 app.use(express.text());
+
+/**
+ * 
+ * @param text - The input to be separated
+ * @returns - An array of words
+ */
+function separateWords(text: string): string[]{
+  const cleanedText: string = cleanText(text);
+  return cleanedText.split(" ");
+}
+
+/**
+ * Removes special characters (exept swedish letters) and converts to lowercase
+ * @param text - the input to be cleaned
+ * @returns {string} - The cleaned text
+ */
+function cleanText(text: string): string {
+  return text.replace(/[^\w\såäöÅÄÖ]/gi, "").toLowerCase();
+}
+
+/**
+ * Counts the word frequency in an array of words. 
+ * @param {string[]} words - The array of words.
+ * @returns {Object} - The word frequency map.
+ */
+function countWordFrequency(words: string[]): { [word: string]: number } {
+  const wordFrequencyMap: { [word: string]: number } = {};
+
+  words.forEach((word) => {
+    if (word !== "") {
+      if (word in wordFrequencyMap) {
+        wordFrequencyMap[word]++;
+      } else {
+        wordFrequencyMap[word] = 1;
+      }
+    }
+  });
+
+  return wordFrequencyMap;
+}
+/**
+ * Sorts the word frequency map by biggest frequency first.
+ * @param wordFrequencyMap - The word frequency map.
+ * @returns - The sorted entries of the word frequency map.
+ */
+function sortWordFrequencyMap(wordFrequencyMap: {
+  [word: string]: number;
+}): [string, number][] {
+  
+  const wordEntries = Object.entries(wordFrequencyMap);
+
+  return wordEntries.sort((a, b) => b[1] - a[1]);
+}
+/**
+ * Retrieves the top word frequency entries from sorted entries.
+ * @param sortedEntries - The sorted entries of the word frequency map.
+ * @param limit - The amount of entries to retrieve
+ * @returns - The top word frequency map.
+ */
+function getTopWordFrequencyByLimit(
+  sortedEntries: [string, number][],
+  limit: number
+): { [word: string]: number } {
+
+  const slicedEntries = sortedEntries.slice(0, limit);
+  
+  const sortedWordFrequency: { [word: string]: number } = {};
+
+  slicedEntries.forEach(([word, frequency]: [string, number]) => {
+    sortedWordFrequency[word] = frequency;
+  });
+  
+  return sortedWordFrequency;
+}
 
 app.post("/count", (req: Request, res: Response, next: NextFunction) => {
   try {
     const text: string = req.body;
 
-    const cleanedText: string = text
-      .replace(/[^\w\såäöÅÄÖ]/gi, "")
-      .toLowerCase();
+    const words: string[] = separateWords(text);
 
-    const words: string[] = cleanedText.split(" ");
+    const wordFrequencyMap = countWordFrequency(words);
 
-    const wordFrequencyMap: { [word: string]: number } = {};
+    const sortedWordFrequencyMap = sortWordFrequencyMap(wordFrequencyMap);
 
-    words.forEach((word) => {
-      if (word !== "") {
-        if (word in wordFrequencyMap) {
-          wordFrequencyMap[word]++;
-        } else {
-          wordFrequencyMap[word] = 1;
-        }
-      }
-    });
+    const topWordFrequency = getTopWordFrequencyByLimit(sortedWordFrequencyMap, 10);
 
-    const sortedWordFrequency: { [word: string]: number } = {};
-
-    const wordEntries = Object.entries(wordFrequencyMap);
-
-    const sortedEntries = wordEntries.sort((a, b) => b[1] - a[1]);
-
-    const slicedEntries = sortedEntries.slice(0, 10);
-
-    slicedEntries.forEach(([word, frequency]: [string, number]) => {
-      sortedWordFrequency[word] = frequency;
-    });
-
-    res.status(200).json(sortedWordFrequency);
+    res.status(200).json(topWordFrequency);
   } catch (error) {
     next(error);
   }
